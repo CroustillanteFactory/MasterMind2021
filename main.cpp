213366b2c3fc453991f0a597847c3f2ef57e8460
L667V <84516568+L667V@users.noreply.github.com>
2021-05-28 17:33:09 +0200

    Add lib

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <graphics.h>
+#include <stdio.h>
+#include <conio.h>
+#include <dos.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    printf("Salut");
     return 0;
 }
